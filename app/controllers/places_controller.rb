eabd58b10833562a0de8dac8345e86cc30ebbf9e
Marzena Szopinska <marzena_szopinska@hotmail.com>
2017-01-30 17:41:19 +0000

    listed places in my database on my index page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
 
   def index
-    
+    # all the places in our database are put into the variable called @places
+    @places = Place.all
   end
 
 end
